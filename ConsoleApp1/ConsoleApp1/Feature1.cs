9e2384214c517942a16c43b95a38ccb2f608529b
Sheldon Schermandt <sheldon.schermandt@live.co.za>
2020-03-17 16:50:31 +0200

    Refactored the Add Method

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
-    class Feature1
+    internal class Feature1
     {
-        public int Add()
+        public int Add(int x1, int x2)
         {
-            int x1 = 1;
-            int x2 = 2;
-            int sum = x1 + x2;
-            return sum;
+            return x1 + x2;
         }
     }
 }
